909fb3df7f27c752644cf3d907e7496729c92592
mark-sprott <sprotty01@gmail.com>
2018-02-19 22:39:43 +1300

    code added to make div four visible from invisible

--- classes.js
+++ classes.js
@@ -6,7 +6,10 @@ document.addEventListener('DOMContentLoaded', start)
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
   // one()
-
+  one()
+  two()
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 
 
@@ -19,20 +22,29 @@ var one = document.getElementById('one')
 
 
 // CREATE FUNCTION two HERE
+}
 
+function one () {
+  // First, we have to find the element:
+  var one = document.getElementById('one')
 
-var two = document.getElementById('two')
-two.classList.add('green')
+  // Next, we apply a new CSS class to it:
+  one.classList.add('blue')
+}
 
-
+// CREATE FUNCTION two HERE
+function two() {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 // CREATE FUNCTION three HERE
-
-
-var three = document.getElementById('three')
-three.classList.add('black')
+function three() {
+  var three = document.getElementById('three')
+  three.classList.add('pink')
 }
 // CREATE FUNCTION makeVisible HERE
 function makeVisible() {
-    document.getElementById("invisible");
-    invisible.classList.add('red')
+  var four = document.getElementsByClassName('invisible')
+  four[0].classList.add('visible')
+
 }
