b7e0ba5efec285c0d7ae8b35204c1c475cc71a13
mark-sprott <sprotty01@gmail.com>
2018-02-19 22:41:45 +1300

    function added to make invisible div visible

--- classes.js
+++ classes.js
@@ -5,23 +5,11 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-  one()
-  two()
-  three()
-  makeVisible()
+one()
+two()
+three()
+makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
-
-
-
-  // First, we have to find the element:
-var one = document.getElementById('one')
-
-  // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
-
-
-// CREATE FUNCTION two HERE
 }
 
 function one () {
